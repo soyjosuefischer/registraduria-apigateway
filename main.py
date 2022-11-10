317094bc37fa4cdec0a39eaaa6f36be6efdc82a5
Laguillo <laguillo@gmail.com>
2022-11-10 18:18:06 -0500

    Fix config

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
 from flask import Flask
 from flask import jsonify
 from flask import request
@@ -9,34 +13,33 @@ import requests
 import re
 
 
-app=Flask(__name__)
+app = Flask(__name__)
 cors = CORS(app)
 
-    #implementacion de login
-from flask_jwt_extended import create_access_token, verify_jwt_in_request
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
+# implementacion de login
 
-app.config["JWT_SECRET_KEY"]="code-geeks"
+app.config["JWT_SECRET_KEY"] = "code-dev"
 jwt = JWTManager(app)
+
 
 @app.route("/login", methods=["POST"])
 def create_token():
     data = request.get_json()
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    url=dataConfig["url-backend-seguridad"]+'/usuarios/validar'
+    url = dataConfig["url-backend-seguridad"]+'/usuarios/validar'
     response = requests.post(url, json=data, headers=headers)
     if response.status_code == 200:
         user = response.json()
         expires = datetime.timedelta(seconds=60 * 60*24)
-        access_token = create_access_token(identity=user,expires_delta=expires)
+        access_token = create_access_token(
+            identity=user, expires_delta=expires)
         return jsonify({"token": access_token, "user_id": user["_id"]})
     else:
         return jsonify({"msg": "Error en usuario o contrasena"}), 401
 
+    # Middleware
 
-    #Middleware
+
 @app.before_request
 def before_request_callback():
     endPoint = limpiarURL(request.path)
@@ -47,11 +50,13 @@ def before_request_callback():
     elif verify_jwt_in_request():
         usuario = get_jwt_identity()
         if usuario["rol"] is not None:
-            tienePersmiso = validarPermiso(endPoint, request.method, usuario["rol"]["_id"])
+            tienePersmiso = validarPermiso(
+                endPoint, request.method, usuario["rol"]["_id"])
             if not tienePersmiso:
                 return jsonify({"message": "Permiso denegado"}), 401
     else:
         return jsonify({"message": "Permiso denegado"}), 401
+
 
 def limpiarURL(url):
     partes = request.path.split("/")
@@ -60,25 +65,28 @@ def limpiarURL(url):
             url = url.replace(laParte, "?")
     return url
 
+
 def validarPermiso(endPoint, metodo, idRol):
-    url = dataConfig["url-backend-seguridad"] + "/permisos-roles/validar-permiso/rol/"+str(idRol)
+    url = dataConfig["url-backend-seguridad"] + \
+        "/permisos-roles/validar-permiso/rol/"+str(idRol)
     tienePermiso = False
     headers = {"Content-Type": "application/json; charset=utf-8"}
     body = {
-        "url":endPoint,
-        "metodo":metodo
+        "url": endPoint,
+        "metodo": metodo
     }
-    response = requests.get(url,json=body, headers=headers)
+    response = requests.get(url, json=body, headers=headers)
     try:
-        data=response.json()
-        if("_id" in data):
-            tienePermiso=True
+        data = response.json()
+        if ("_id" in data):
+            tienePermiso = True
     except:
         pass
     return tienePermiso
 
+    # Redireccionamiento CRUD candidatos
 
-    #Redireccionamiento CRUD candidatos
+
 @app.route("/candidatos", methods=['GET'])
 def getCandidatos():
     headers = {"Content-Type": "application/json; charset=utf-8"}
@@ -86,6 +94,7 @@ def getCandidatos():
     response = requests.get(url, headers=headers)
     json = response.json()
     return jsonify(json)
+
 
 @app.route("/candidatos", methods=['POST'])
 def crearCandidato():
@@ -96,6 +105,7 @@ def crearCandidato():
     json = response.json()
     return jsonify(json)
 
+
 @app.route("/candidatos/<string:id>", methods=['GET'])
 def getCandidato(id):
     headers = {"Content-Type": "application/json; charset=utf-8"}
@@ -103,6 +113,7 @@ def getCandidato(id):
     response = requests.get(url, headers=headers)
     json = response.json()
     return jsonify(json)
+
 
 @app.route("/candidatos/<string:id>", methods=['PUT'])
 def modificarCandidato(id):
@@ -113,6 +124,7 @@ def modificarCandidato(id):
     json = response.json()
     return jsonify(json)
 
+
 @app.route("/candidatos/<string:id>", methods=['DELETE'])
 def eliminarCandidato(id):
     headers = {"Content-Type": "application/json; charset=utf-8"}
@@ -121,8 +133,8 @@ def eliminarCandidato(id):
     json = response.json()
     return jsonify(json)
 
-
     # Redireccionamiento CRUD Mesas
+
     @app.route("/mesas", methods=['GET'])
     def getMesas():
         headers = {"Content-Type": "application/json; charset=utf-8"}
@@ -164,7 +176,6 @@ def eliminarCandidato(id):
         response = requests.delete(url, headers=headers)
         json = response.json()
         return jsonify(json)
-
 
         # Redireccionamiento CRUD Partidos
 
@@ -210,7 +221,6 @@ def eliminarCandidato(id):
         json = response.json()
         return jsonify(json)
 
-
         # Redireccionamiento CRUD Resultados
 
     @app.route("/resultados", methods=['GET'])
@@ -229,14 +239,14 @@ def eliminarCandidato(id):
         json = response.json()
         return jsonify(json)
 
-
     # relacion mesas-candidatos a resultados
 
     @app.route("/resultados/mesas/<string:id_mesa>/candidatos/<string:id_candidato>", methods=['POST'])
     def crearResultado(id_mesa, id_candidato):
         data = request.get_json()
         headers = {"Content-Type": "application/json; charset=utf-8"}
-        url = dataConfig["url-backend-registraduria"] + '/resultados/mesas/' + id_mesa + '/candidatos/' + id_candidato
+        url = dataConfig["url-backend-registraduria"] + \
+            '/resultados/mesas/' + id_mesa + '/candidatos/' + id_candidato
         response = requests.post(url, headers=headers, json=data)
         json = response.json()
         return jsonify(json)
@@ -253,11 +263,11 @@ def eliminarCandidato(id):
     def modificarResultado(id_resultado, id_mesa, id_candidato):
         data = request.get_json()
         headers = {"Content-Type": "application/json; charset=utf-8"}
-        url = dataConfig["url-backend-registraduria"] + '/resultados/' + id_resultado + '/mesas/' + id_mesa + '/candidatos/' + id_candidato
+        url = dataConfig["url-backend-registraduria"] + '/resultados/' + \
+            id_resultado + '/mesas/' + id_mesa + '/candidatos/' + id_candidato
         response = requests.put(url, headers=headers, json=data)
         json = response.json()
         return jsonify(json)
-
 
     # Relacion candidato partido
 
@@ -265,29 +275,32 @@ def eliminarCandidato(id):
     def asignarPartidoACandidato(id, id_partido):
         data = request.get_json()
         headers = {"Content-Type": "application/json; charset=utf-8"}
-        url = dataConfig["url-backend-registraduria"] + '/candidatos/' + id + '/partidos/' + id_partido
+        url = dataConfig["url-backend-registraduria"] + \
+            '/candidatos/' + id + '/partidos/' + id_partido
         response = requests.put(url, headers=headers, json=data)
         json = response.json()
         return jsonify(json)
 
+    # servidor corriendo
 
-    #servidor corriendo
 
-@app.route("/",methods=['GET'])
+@app.route("/", methods=['GET'])
 def test():
     json = {}
-    json["message"]="Server running ..."
+    json["message"] = "Server running ..."
     return jsonify(json)
 
+    # config
 
-    #config
 
 def loadFileConfig():
     with open('config.json') as f:
         data = json.load(f)
     return data
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     dataConfig = loadFileConfig()
-    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
-    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
+    print("Server running : "+"http://" +
+          dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app, host=dataConfig["url-backend"], port=dataConfig["port"])
